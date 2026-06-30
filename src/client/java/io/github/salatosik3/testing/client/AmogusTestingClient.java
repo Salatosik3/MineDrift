a1fcde35119781d8ffed5310fc4f4680b2de13bc
Salatosik3 <banserukvitalii@gmail.com>
2026-07-01 01:48:13 +0200

    First try to render something on the client side (woah)

--- src/client/java/io/github/salatosik3/testing/client/AmogusTestingClient.java
+++ src/client/java/io/github/salatosik3/testing/client/AmogusTestingClient.java
@@ -1,11 +1,23 @@
 package io.github.salatosik3.testing.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AmogusTestingClient implements ClientModInitializer {
+	public static final String MOD_ID = "amogustesting";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
 	@Override
 	public void onInitializeClient() {
+		HudElementRegistry.addFirst(Identifier.fromNamespaceAndPath(MOD_ID, "trying_to_figure_it_out"),
+				(graphics, deltaTracker) -> {
+			graphics.textRenderer().accept(graphics.guiWidth() / 2, graphics.guiHeight() / 2, Component.literal("Go fuck yourself."));
+		});
 
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
--- src/main/java/io/github/salatosik3/testing/AmogusTesting.java
+++ src/main/java/io/github/salatosik3/testing/AmogusTesting.java
@@ -5,11 +5,14 @@ import io.github.salatosik3.testing.listener.bus.BusEventListenerManager;
 import io.github.salatosik3.testing.listener.fabric.EventListenerManager;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElement;
 import net.minecraft.resources.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+// TODO I think it will be so laggy if I try to send packets continuosly until a player is drifting, so I will have to make third module in the project that connects two sides (it is needed because of code organization, I want things to be organized)
 public class AmogusTesting implements ModInitializer {
 	public static final String MOD_ID = "amogustesting";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
