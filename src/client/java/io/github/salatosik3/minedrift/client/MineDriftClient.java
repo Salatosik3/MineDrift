83250617eecd12470c01bfec283906ed25171839
Salatosik3 <banserukvitalii@gmail.com>
2026-07-03 01:01:44 +0200

    I successfuly learned how to render things and do whatever I want without breaking ingame HUD using push/pop pose matrix and using translate method to keep it on the right place

--- src/client/java/io/github/salatosik3/minedrift/client/MineDriftClient.java
+++ src/client/java/io/github/salatosik3/minedrift/client/MineDriftClient.java
@@ -4,6 +4,10 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +15,32 @@ public class MineDriftClient implements ClientModInitializer {
 	public static final String MOD_ID = "minedrift";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private float rotation = 0;
 
 	@Override
 	public void onInitializeClient() {
+
 		HudElementRegistry.addFirst(Identifier.fromNamespaceAndPath(MOD_ID, "trying_to_figure_it_out"),
 				(graphics, deltaTracker) -> {
-			graphics.textRenderer().accept(graphics.guiWidth() / 2, graphics.guiHeight() / 2, Component.literal("Go fuck yourself."));
+
+			var pose = graphics.pose();
+			pose.pushMatrix();
+
+//			graphics.textRenderer().accept(graphics.guiWidth() / 2, graphics.guiHeight() / 2, Component.literal(String.valueOf(deltaTracker.getGameTimeDeltaPartialTick(true))));
+//			graphics.textRenderer().accept(graphics.guiWidth() / 2, graphics.guiHeight() / 2, Component.literal("Go fuck yourself."));
+
+			var centerX = graphics.guiWidth() / 2;
+			var centerY = graphics.guiHeight() / 2;
+
+			pose.translate(centerX, centerY);
+
+			rotation += 0.01f;
+			pose.rotate(rotation);
+
+//			pose.scale(2, 2);
+
+			graphics.fakeItem(new ItemStack(Items.ACACIA_BOAT, 1), 0, 0);
+			pose.popMatrix();
 		});
 
 	}
