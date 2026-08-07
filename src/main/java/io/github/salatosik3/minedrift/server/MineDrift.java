f1a4be1bd2ab8d72f2c5cda086eb5d292ad24a15
Salatosik3 <banserukvitalii@gmail.com>
2026-08-07 12:00:54 +0200

    Trying to glue two services to work correctly

--- src/main/java/io/github/salatosik3/minedrift/server/MineDrift.java
+++ src/main/java/io/github/salatosik3/minedrift/server/MineDrift.java
@@ -14,6 +14,7 @@ import net.minecraft.server.players.PlayerList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// TODO think about cleaning maps/lists in some classes that stores a data depending on player/entity UUIDs (this will be a huge problem with a large amount of players)
 public class MineDrift implements ModInitializer {
 	public static final String MOD_ID = "minedrift";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
---
+++ src/main/java/io/github/salatosik3/minedrift/server/service/State.java
@@ -0,0 +1,5 @@
+package io.github.salatosik3.minedrift.server.service;
+
+public enum State {
+    DRIFT_COOLDOWN, DRIFT, NO_DRIFT
+}
